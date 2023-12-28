87be2118e4ce9671ad816815297cd8ef1f44a7ac
no2thingmore <nangmuu00@gmail.com>
2023-12-28 18:45:46 +0900

    Intro | 프론트 제작중, Header, Footer 완성

---
+++ src/component/mainpage/aboute/index.js
@@ -0,0 +1,13 @@
+import './about.css';
+
+function About() {
+    return (
+        <>
+            <div className='a_container'>
+                
+            </div>
+        </>
+    )
+}
+
+export default About;
---
+++ src/component/mainpage/repository/index.js
@@ -0,0 +1,9 @@
+function Repository() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Repository;
