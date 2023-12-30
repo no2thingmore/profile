b802c3452894c0fb647d0481cde0307231ed51ee
no2thingmore <nangmuu00@gmail.com>
2023-12-30 14:32:57 +0900

    About | 프론트 완료

--- src/component/mainpage/skill/index.js
+++ src/component/mainpage/skill/index.js
@@ -1,7 +1,15 @@
+import './skill.css'
+
 function Skill() {
     return (
         <>
-            
+            <div>
+                <div className='sk_title'>
+                    Skill
+                </div>
+                <div>
+                </div>
+            </div>
         </>
     )
 }
